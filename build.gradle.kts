a6ec83a42be3fe0457086cd36109d23c9f0b9505
Nikolay Irtuganov <vongu3@gmail.com>
2025-02-15 13:44:32 +0300

    session1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation("org.jetbrains.exposed:exposed-core:0.57.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.57.0")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.57.0")
 }
 
 compose.desktop {
